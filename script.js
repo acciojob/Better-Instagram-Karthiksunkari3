da341c2c762982c593e2d20a5cb42313c96da7f4
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-17 00:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+// Attach the draggable and droppable events to the divs.
+document.querySelectorAll(".draggable").forEach(div => {
+  div.addEventListener("dragstart", e => {
+    e.dataTransfer.setData("text/html", div.innerHTML);
+  });
+
+  div.addEventListener("drop", e => {
+    const draggedDiv = e.target;
+    const droppedDiv = div;
+
+    // Swap the contents of the dragged and dropped divs.
+    draggedDiv.innerHTML = droppedDiv.innerHTML;
+    droppedDiv.innerHTML = e.dataTransfer.getData("text/html");
+  });
+});
